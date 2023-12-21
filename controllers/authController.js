96626c9912f55038212355142f72fbeec0ac33ba
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2023-12-21 14:55:27 +0530

    changed res.send in authController from error to message

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,102 +1,109 @@
 import userModel from "../models/userModel.js";
-import {comparePassword, hashPassword} from  "../helpers/authHelper.js"
-import jwt from "jsonwebtoken"
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import jwt from "jsonwebtoken";
 
 // REGISTER || POST
-export const registerController = async(req,res) => {
-    try {
-        const {name, email, password, address, phone} = req.body;
-        // Validation
-        if(!name){
-            return res.send({error: "Name is required"})
-        }
-        if(!email){
-            return res.send({error: "Email is required"})
-        }
-        if(!phone){
-            return res.send({error: "Phone is required"})
-        }
-        if(!password){
-            return res.send({error: "Password is required"})
-        }
-        if(!address){
-            return res.send({error: "Address is required"})
-        }
-
-        // Check existing user
-        const existingUser = await userModel.findOne({email})
-        if(existingUser){
-            return res.status(200).send({
-                success: true,
-                message: "Already registered please login"
-            })
-        }
-        // Register User
-        const hashedPassword = await hashPassword(password);
-        const user = new userModel({name, email, phone, address, password: hashedPassword});
-        user.save();
-        res.status(200).send({
-            success: true,
-            message: "Registered successfully",
-            user
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({
-            success: false,
-            message: "Internal Server Error",
-            error
-        })
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, address, phone } = req.body;
+    // Validation
+    if (!name) {
+      return res.send({ message: "Name is required" });
     }
-}
+    if (!email) {
+      return res.send({ message: "Email is required" });
+    }
+    if (!phone) {
+      return res.send({ message: "Phone is required" });
+    }
+    if (!password) {
+      return res.send({ message: "Password is required" });
+    }
+    if (!address) {
+      return res.send({ message: "Address is required" });
+    }
 
+    // Check existing user
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "Already registered please login",
+      });
+    }
+    // Register User
+    const hashedPassword = await hashPassword(password);
+    const user = new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPassword,
+    });
+    user.save();
+    res.status(200).send({
+      success: true,
+      message: "Registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
 
 // LOGIN || POST
-export const loginController = async(req,res) => {
-    try {
-        const {email, password} = req.body;
-        // validation
-        if(!email || !password){
-            return res.send({
-                success: false,
-                message: "Invalid email or password"
-            })
-        } 
-        // check user
-        const user =  await userModel.findOne({email});
-        if(!user){
-            return res.status(404).send({
-                success: false,
-                message: "Invalid email or password"
-            })
-        }
-        const passwordVerified = await comparePassword(password, user.password)
-        if(!passwordVerified){
-            return res.status(200).send({
-                success: false,
-                message: "Invalid email or password"
-            })
-        }
-        // jwt token
-        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: "7d"});
-        res.status(200).send({
-            success: true,
-            message: "login successful",
-            user: {
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                phone: user.phone,
-                address: user.address
-            },
-            token
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({
-            success: false,
-            message: "Internal Server Error",
-            error
-        })
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.send({
+        success: false,
+        message: "Invalid email or password",
+      });
     }
-}
+    // check user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    const passwordVerified = await comparePassword(password, user.password);
+    if (!passwordVerified) {
+      return res.status(200).send({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+    // jwt token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(200).send({
+      success: true,
+      message: "login successful",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Internal Server Error",
+      error,
+    });
+  }
+};
