0671d110f72fc4334bd973d3cb4a9fe3b0c21ce3
Abhishek Maurya <abhishekmaurya.official@gmail.com>
2023-12-21 15:48:57 +0530

    added logic for protected routes and removed test route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,20 +1,21 @@
-import express from "express"
-import {loginController, registerController }from "../controllers/authController.js";
+import express from "express";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 
-const router =  express.Router()
+const router = express.Router();
 
 // REGISTER || POST
 router.post("/register", registerController);
 
 // LOGIN || POST
-router.post("/login", loginController)
+router.post("/login", loginController);
 
-// test route
-router.get("/test",requireSignIn,isAdmin,(req,res,next) => {
-    res.send({
-        message: "this is secure route"
-    })
-})
+// Protected routes
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
 
 export default router;
